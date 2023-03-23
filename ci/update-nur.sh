d709fc150faf2c181c5a5a98727952ab8d544296
Milan Hauth <milahu@gmail.com>
2023-03-23 09:20:36 +0100

    allow unbound variables

--- ci/update-nur.sh
+++ ci/update-nur.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -p git -p nix -p bash -i bash
 
-set -eu -o pipefail # Exit with nonzero exit code if anything fails
+set -e -o pipefail # Exit with nonzero exit code if anything fails
 
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
