a5e93d4418c4ae9f987e12614b7b38b9537a224e
Milan Hauth <milahu@gmail.com>
2023-08-06 21:22:28 +0200

    update git-worktree-add.sh

--- scripts/git-worktree-add.sh
+++ scripts/git-worktree-add.sh
@@ -2,6 +2,14 @@
 
 set -x
 
-for branch in gh-pages nur-combined nur-eval-errors nur-repos nur-repos-lock nur-search; do
+for branch in gh-pages nur-combined nur-eval-errors nur-repos nur-repos-lock nur-search nur-search-data; do
   git worktree add $branch $branch
+
+  # fetch submodules
+  # nur-search has a git module for hugo theme
+  git -C $branch submodule update --init --depth=1 --recursive
 done
+
+# unmount branch:
+# git -C $branch submodule deinit --all --force
+# git worktree remove $branch --force
