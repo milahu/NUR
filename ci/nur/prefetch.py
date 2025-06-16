2601a492d73ce0d5f616c20447c787e215b4af77
Milan Hauth <milahu@gmail.com>
2025-06-16 14:05:00 +0200

    Reapply "limit to 300 github_repos"
    This reverts commit 36052c0d2ea46709c8b9b4a978e759fb09c158f5.

--- ci/nur/prefetch.py
+++ ci/nur/prefetch.py
@@ -463,8 +463,10 @@ async def update_version_github_repos(repos, aiohttp_session, filter_repos_fn):
         query = "query{\n"
         done_first = False
         # TODO future: split into multiple smaller queries
+        # 338 repos are too many -> always returns http 502 bad gateway
         # TODO what about non-github repos? use graphQL API of codeberg.org etc?
-        for repo_id, repo in enumerate(github_repos):
+        #for repo_id, repo in enumerate(github_repos):
+        for repo_id, repo in enumerate(github_repos[0:300]):
             if done_first:
                 query += ",\n"
             else:
