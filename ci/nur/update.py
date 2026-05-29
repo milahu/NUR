6f517a10b209ec55bfb4dead981a392a21ec197f
Milan Hauth <milahu@milahu.duckdns.org>
2026-05-29 22:58:33 +0200

    skip the first blocks of the trace

--- ci/nur/update.py
+++ ci/nur/update.py
@@ -348,6 +348,43 @@ def update_command_inner(args: Namespace) -> None:
             if main_err_line == None:
                 main_err_line = err.stdout
 
+            # skip the first blocks of the trace
+            #
+            # error:
+            # … while evaluating the file '/tmp/nur-update/default.nix':
+            # … from call site
+            #   at /tmp/nur-update/default.nix:3:21:
+            # … while calling anonymous lambda
+            #   at /tmp/nur-update/evalRepo.nix:1:1:
+            # … from call site
+            #   at /tmp/nur-update/evalRepo.nix:33:11:
+            #
+            # --- the user repo starts here ---
+            #
+            # … while calling anonymous lambda
+            #   at /nix/store/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-nur-packages/default.nix:1:1:
+
+            start = 0
+            second_line_prefix = "  at /nix/store/"
+            logger.debug(f"trace: repo.file: {repo.file!r}")
+            logger.debug(f"trace: second_line_prefix: {second_line_prefix!r}")
+            for line_idx, line in enumerate(err_lines):
+                logger.debug(f"trace: line {line_idx}: {line!r}")
+                if line.startswith(second_line_prefix):
+                    # line_idx-1: … while calling anonymous lambda
+                    # line_idx-0:   at /nix/store/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-nur-packages/default.nix:1:1:
+                    start = line_idx - 1
+                    logger.debug(f"trace: found second_line_prefix in line {line_idx}")
+                    break
+                if line_idx > 50:
+                    # give up
+                    # the wanted line should be at line_idx=24
+                    break
+            if start > 0:
+                logger.debug(f"trace: skipping the first {start} lines")
+                err_lines = ["error:"] + err_lines[start:]
+                err.stdout = "\n".join(err_lines) + "\n"
+
             if repo.locked_version is None:
                 # repo was added only to repos.json but not to repos.json.lock
 
