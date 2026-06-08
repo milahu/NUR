2495bed78d70e62975c4b0f813ce54981e0bcc5a
Milan Hauth <milahu@milahu.duckdns.org>
2026-06-08 20:14:07 +0200

    add git-branch-size.sh

---
+++ scripts/git-branch-size.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# https://stackoverflow.com/questions/32557849
+
+git for-each-ref --format='%(refname)' |
+grep ^refs/heads/ |
+grep -v -E '^refs/heads/(bak|trash)-' |
+while read branch
+do
+    root_commit=$(git log --reverse --format=%H $branch | head -n1)
+    size=$(
+        git rev-list --disk-usage --objects $root_commit..$branch |
+        LC_ALL=C numfmt --to=iec-i --suffix=B --format=%.2f
+    )
+    branch=${branch:11}
+    echo $size$'\t'$branch
+done |
+LC_ALL=C sort -h
